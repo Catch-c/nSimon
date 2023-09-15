6cec3278026cc64fc8214da43b21064f09f26b97
catch-c <80603287+Catch-c@users.noreply.github.com>
2023-09-15 18:24:02 +1000

    Daily Messages Updated, Main Design Updated, Class Resources Updated

--- Start.py
+++ Start.py
@@ -202,10 +202,10 @@ def home():
     return render_template('home.html')
 
 
-if __name__ == "__main__":
-    from waitress import serve
-    print("STARTED!")
-    serve(app, host="0.0.0.0", port=8080)
+# if __name__ == "__main__":
+#     from waitress import serve
+#     print("STARTED!")
+#     serve(app, host="0.0.0.0", port=8080)
 
-# if __name__ == '__main__':
-#     app.run(debug=True,  port=8080)
+if __name__ == '__main__':
+    app.run(debug=True,  port=8080)
