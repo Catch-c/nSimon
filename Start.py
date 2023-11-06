750dbe418bf71b78d15f5e70f71694311df51936
catch-c <80603287+Catch-c@users.noreply.github.com>
2023-11-06 16:23:26 +1100

    2.0.0-1

--- Start.py
+++ Start.py
@@ -27,7 +27,7 @@ flask_secret = config.get('Flask', 'secret')
 # --[[ Flask Setup ]]--
 app = Flask(__name__)
 app.secret_key = flask_secret
-VERSION = "1.3.9"
+VERSION = "2.0.0"
 
 # --[[ API Routes ]]--
 #       --[[ getTimetable () ]]--
