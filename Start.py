e65cb60978f3018aa1c3c584aae0fb274a70fd79
catch-c <80603287+Catch-c@users.noreply.github.com>
2023-11-11 19:44:35 +1100

    2.2.2

--- Start.py
+++ Start.py
@@ -134,11 +134,11 @@ app.register_blueprint(supportBlueprint)
 
 # --[[ Start ]]--
 #       --[[ PRODUCTION ]]--
-# if __name__ == "__main__":
-#     from waitress import serve
-#     logger.info("nSimon Started!")
-#     serve(app, host="0.0.0.0", port=8080)
+if __name__ == "__main__":
+    from waitress import serve
+    logger.info("nSimon Started!")
+    serve(app, host="0.0.0.0", port=8080)
 
 #       --[[ TESTING ]]--
-if __name__ == "__main__":
-    app.run(debug=True, port=8080)
+# if __name__ == "__main__":
+#     app.run(debug=True, port=8080)
